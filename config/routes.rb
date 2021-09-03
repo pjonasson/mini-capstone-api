a374b2379cc6f4c1969f918878f90b42d7a4afb4
Percy Jonasson <pjonasson@outlook.com>
2021-09-03 09:27:00 -0500

    Generated model Order to begin purchasing process

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 
   get "/products" => "products#index"
   post "/products" => "products#create"
