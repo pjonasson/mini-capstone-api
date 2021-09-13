c33eaae42566faa72a8068a508f45f8e92b6a6a7
Percy Jonasson <pjonasson@outlook.com>
2021-09-13 09:53:46 -0500

    Fixed errors with authenticate.admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,4 +26,6 @@ class ApplicationController < ActionController::API
   def authenticate_admin
     unless current_user && current_user.authenticate_admin
       render json: {}, status: :unauthorized
+    end
+  end
 end
