58a1432a811d977b23d2ddf46565fa76ad8b6403
Percy Jonasson <pjonasson@outlook.com>
2021-09-15 09:42:31 -0500

    Removed Product_Id and Quantity from Order model using migrations

---
+++ db/migrate/20210915143611_remove_product_id_from_order_model.rb
@@ -0,0 +1,5 @@
+class RemoveProductIdFromOrderModel < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :orders, :product_id, :integer
+  end
+end
---
+++ db/migrate/20210915144035_remove_quantity_from_order_model.rb
@@ -0,0 +1,5 @@
+class RemoveQuantityFromOrderModel < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :orders, :quantity, :integer
+  end
+end
